9220dfb0f196159244b20a1747808813c0885c61
liyang <235938019@qq.com>
2019-07-07 09:15:33 +0000

    liyang

---
+++ mfs_install.sh
@@ -0,0 +1,55 @@
+#config local yum source on node1 node2 node3
+cd /etc/yum.repos.d/
+rm -rf *
+cat > /etc/yum.repos.d/hpc_mfs.repo << EOF
+[mfs]
+name=mfs
+baseurl=file:///root/mfs
+enabled=1
+gpgcheck=0
+[hpc]
+name=hpc
+baseurl=file:///root/openhpc
+enabled=1
+gpgcheck=0
+[keepalived]
+name=keepalived
+baseurl=file:///root/keepalived
+enabled=1
+gpgcheck=0
+EOF
+
+ssh node2 "rm -rf /etc/yum.repos.d/*"
+ssh node3 "rm -rf /etc/yum.repos.d/*"
+scp /etc/yum.repos.d/hpc_mfs.repo node2:/etc/yum.repos.d/
+scp /etc/yum.repos.d/hpc_mfs.repo node3:/etc/yum.repos.d/
+
+#config master node
+yum -y install moosefs-master moosefs-cgi moosefs-cgiserv moosefs-cli
+mfsmaster start
+mfscgiserv start
+sleep 5
+echo "mfsmaster -a" >> /etc/rc.local
+echo "mfscgiserv start" >> /etc/rc.local
+echo "sleep 5" >> /etc/rc.local
+
+#config local disk to mfs use
+for i in {1..3};do ssh node$i mkdir /data;done
+for i in {1..3};do ssh node$i mkfs.xfs /dev/vdb;done
+for i in {1..3};do ssh node$i "echo /dev/vdb /data xfs defaults 0 0 >> /etc/fstab";done
+for i in {1..3};do ssh node$i mount -a;done
+
+#config chunkserver on each nodes
+for i in {1..3};do ssh node$i yum -y install moosefs-chunkserver moosefs-client;done
+for i in {1..3};do ssh node$i chown mfs:mfs /data;done
+for i in {1..3};do ssh node$i "echo MASTER_HOST=node1 >> /etc/mfs/mfschunkserver.cfg";done
+for i in {1..3};do ssh node$i "echo /data >> /etc/mfs/mfshdd.cfg";done
+for i in {1..3};do ssh node$i mfschunkserver start;done
+for i in {1..3};do ssh node$i "echo mfschunkserver start >> /etc/rc.local";done
+
+#config mfs client mount
+for i in {1..3};do ssh node$i mkdir /mfs;done
+for i in {1..3};do ssh node$i mfsmount -H node1 /mfs;done
+for i in {1..3};do ssh node$i "echo mfsmount -H node1 /mfs >> /etc/rc.local";done
+
+for i in {1..3};do ssh node$i chmod 755 /etc/rc.d/rc.local;done
